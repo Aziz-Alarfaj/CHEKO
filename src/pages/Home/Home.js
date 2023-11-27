6d2509dd109a0f5483742354a67b64317d4c15f3
Aziz-Alarfaj <azs.alarfaj@gmail.com>
2023-11-27 20:02:25 +0300

    re-styled category cards

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -23,26 +23,25 @@ const Home = () => {
                         <List
                             style={{
                                 overflowX: 'auto',
+                                overflowY: 'hidden',
                                 maxHeight: '150px',
+                                whiteSpace: 'nowrap',
                             }}
-                            // grid={{
-                            //     gutter: 36,
-                            //     column: categories.length,
-                            // }}
+
                             dataSource={categories}
                             renderItem={(item) => (
-                                <List.Item>
-                                    <Card style={{ width: '300px', marginRight: '100px' }} onClick={() => setSelectedCategory(item.category)} hoverable={true}>
+                                <List.Item style={{ display: 'inline-block', marginRight: '16px' }}>
+                                    <Card style={{ width: '300px' }} onClick={() => setSelectedCategory(item.category)} hoverable={true}>
                                         <Row gutter={[16, 5]}>
-                                            <Col xs={8} sm={8} md={8} lg={8} xl={8}>
+                                            <Col span={8}>
                                                 <div style={{ backgroundColor: 'pink', borderRadius: '10px', height: '50px', width: '50px' }}>
 
                                                 </div>
                                             </Col>
-                                            <Col xs={8} sm={8} md={8} lg={8} xl={8} style={{ display: 'flex', justifyContent: 'center', margin: 'auto' }}>
+                                            <Col span={8} style={{ display: 'flex', justifyContent: 'center', margin: 'auto' }}>
                                                 <p style={{ fontWeight: 'bold' }}>{item.category}</p>
                                             </Col>
-                                            <Col xs={8} sm={8} md={8} lg={8} xl={8} style={{ display: 'flex', justifyContent: 'center', margin: 'auto' }}>
+                                            <Col span={8} style={{ display: 'flex', justifyContent: 'center', margin: 'auto' }}>
                                                 <p style={{ fontWeight: 'bold' }}>{item.count}</p>
                                             </Col>
                                         </Row>
@@ -52,21 +51,21 @@ const Home = () => {
                         />
                     </Row>
                 </Col>
-                <Col xs={1} sm={1} md={1} lg={1} xl={1} style={{ display: 'flex', justifyContent: 'center', padding: '0px' }}>
+                <Col span={1} style={{ display: 'flex', justifyContent: 'center', padding: '0px' }}>
                     <Divider type="vertical" style={{ height: '85%', borderInlineStartWidth: '3px' }} />
                 </Col>
-                <Col xs={4} sm={4} md={4} lg={4} xl={4}>
+                <Col span={4} style={{ display: 'grid', alignItems: 'center' }}>
 
                     <Card hoverable={true}>
                         <Row gutter={[8, 8]}>
-                            <Col xs={8} sm={8} md={8} lg={8} xl={8}>
+                            <Col span={8}>
                                 <div style={{ backgroundColor: 'pink', borderRadius: '10px', height: '50px', width: '50px' }}>
                                 </div>
                             </Col>
-                            <Col xs={8} sm={8} md={8} lg={8} xl={8} style={{ display: 'flex', justifyContent: 'center', margin: 'auto' }}>
+                            <Col span={8} style={{ display: 'flex', justifyContent: 'center', margin: 'auto' }}>
                                 <p style={{ fontWeight: 'bold' }}>Orders</p>
                             </Col>
-                            <Col xs={8} sm={8} md={8} lg={8} xl={8} style={{ display: 'flex', justifyContent: 'center', margin: 'auto' }}>
+                            <Col span={8} style={{ display: 'flex', justifyContent: 'center', margin: 'auto' }}>
                                 <p style={{ fontWeight: 'bold' }}>3</p>
                             </Col>
                         </Row>
