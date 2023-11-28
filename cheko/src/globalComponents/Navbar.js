eac1bda2cc61f29f9ce21cb4cef92d4e736ed593
Aziz-Alarfaj <azs.alarfaj@gmail.com>
2023-11-28 15:24:06 +0300

    fixed default selected tab in navbar

--- cheko/src/globalComponents/Navbar.js
+++ cheko/src/globalComponents/Navbar.js
@@ -1,18 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Menu, Switch } from 'antd';
 
 
 
 const NavBar = ({ darkMode }) => {
 
+    const location = useLocation();
+
     const items = [
         {
             label: <Link to="/">Home</Link>,
-            key: 'home',
+            key: '/',
         },
         {
             label: <Link to="/map">Map</Link>,
-            key: 'map',
+            key: '/map',
         },
         {
             label: <Switch onClick={darkMode} />,
@@ -24,7 +26,7 @@ const NavBar = ({ darkMode }) => {
         <Menu
             style={{ height: '135%', width: '98%', borderRadius: '0px 0px 36px 0px', backgroundColor: '#242633', paddingBottom: '32px', paddingLeft: '10vw' }}
             mode="horizontal"
-            defaultSelectedKeys={['home']}
+            defaultSelectedKeys={[location.pathname]}
             items={items}
         />
     );
